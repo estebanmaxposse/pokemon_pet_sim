a472d37be94c5f5786c5656882c7e341681c7d43
estebanmaxposse <estebanposse98@gmail.com>
2022-05-10 20:41:45 -0300

    Added basic demo funcionalities using if statementes, reiterations and funcionts.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -21,17 +21,25 @@ alert("So his name is " + pokemonName + "!");
 
 alert(playerName + "! " + pokemonName + "! " +"Your very own Pokémon legend is about to unfold! A world of dreams and adventures with Pokémon awaits! Let's go!")
 
+alert("You can stop playing by typing ESC.")
+
 function getRandomInt(min, max) {
   min = Math.ceil(0);
   max = Math.floor(100);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function mySubstraction(res, num) {
+  let x = res - num;
+  if (x < 0) {
+    x = 0;
+  }
+  return x;
+}
+
 let food = getRandomInt();
 let rest = getRandomInt();
 let fun = getRandomInt();
-
-console.log(food, rest, fun);
 
 for (var day = 1; day <= 7; day++) {
   alert("Day " + day);
@@ -42,40 +50,42 @@ for (var day = 1; day <= 7; day++) {
       food = food + 40;
       alert(food + " points of satiation!");
     }
+    else if (feedResponse === "esc") {
+      alert("Thank you for playing!");
+      break;
+    }
     else {
       alert("Too bad! He seemed to be very hungry.");
-      food = food - 30;
+      food = mySubstraction(food, 30);
       alert(food + " points of satiation.");
     }
   }
   else {
     alert("Pichu seems to be well fed lately. Good job!")
   }
-  food = food - 10;
-  if (food < 0) {
-    food = 0;
-  }
+  food = mySubstraction(food, 30);
 
   if (rest < 40) {
-    const restResponse = prompt(pokemonName + " seems to be tired! Type REST if you want to cuddle him to sleep?")?.toLowerCase();
+    const restResponse = prompt(pokemonName + " seems to be tired! Type REST if you want to cuddle him to sleep.")?.toLowerCase();
     if (restResponse === "rest") {
       alert("He's fast asleep.\n(+20 rest points)");
       rest = rest + 40;
       alert(rest + " points of rest!");
     }
+    else if (restResponse === "esc") {
+      alert("Thank you for playing!");
+      break;
+    }
     else {
       alert("Poor thing, he seems to lack some sleep.");
-      rest = rest - 30;
+      rest = mySubstraction(rest, 20);
       alert(rest + " points of rest.");
     }
   }
   else {
     alert("Pichu is energized as always. Look at him run!")
   }
-  rest = rest - 20;
-  if (rest < 0) {
-    rest = 0;
-  }
+  rest = mySubstraction(rest, 20);
 
   if (fun < 60) {
     const funResponse = prompt(pokemonName + " looks bored Type PLAY if you want to play with him!")?.toLowerCase();
@@ -84,17 +94,23 @@ for (var day = 1; day <= 7; day++) {
       fun = fun + 30;
       alert(fun + " points of fun!");
     }
+    else if (funResponse === "esc") {
+      alert("Thank you for playing!");
+      break;
+    }
     else {
       alert("Maybe some other time...");
-      fun = fun - 40;
+      fun = mySubstraction(fun, 20);
       alert(fun + " points of fun.");
     }
   }
   else {
     alert("Pichu seems happy!")
   }
-  fun = fun - 20;
-  if (fun < 0) {
-    fun = 0;
+  fun = mySubstraction(fun, 20);
+  if (day == 7) {
+    alert("You made it to the end of the week!");
   }
 }
+
+alert("Thank you for playing this short demo of the simulator!\nPress F5 to play again!")
