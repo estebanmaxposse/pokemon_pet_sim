d6836d98a4e87448dc61a42d3949c14674ca074e
estebanmaxposse <estebanposse98@gmail.com>
2022-05-16 17:06:59 -0300

    Code cleaning

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,3 @@
-alert("Pokémon Pet Simulator");
-alert("Hello there! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof!");
-alert("This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights. Myself… I study Pokémon as a profession.");
-
-let playerName;
-playerName = prompt("So, what is your name?") || "Player";
-alert("Right! So your name is " + playerName + "!");
-
 const GENDER_MALE = "male";
 const GENDER_FEMALE = "female";
 
@@ -35,11 +27,26 @@ class Pokemon {
     //once I know how to get sprites from APIs I'll finish this function
     this.get_sprite = function() {
       if (this.name === "pichu") {
-        return console.log("https://i.imgur.com/ROK2yzd.png")
+        return "https://i.imgur.com/ROK2yzd.png";
       }
     }
   }
 }
+
+class Day {
+  constructor(number, x) {
+    this.number = parseInt(number);
+    this.event = events[parseInt(x)];
+  }
+}
+
+alert("Pokémon Pet Simulator");
+alert("Hello there! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof!");
+alert("This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights. Myself… I study Pokémon as a profession.");
+
+let playerName;
+playerName = prompt("So, what is your name?") || "Player";
+alert("Right! So your name is " + playerName + "!");
 
 const pichu = new Pokemon("Pichu", GENDER_MALE, "no_sprite_yet", 124, 2, 24, 56, 99);
 console.log(pichu);
@@ -49,9 +56,9 @@ alert("First, let me introduce you to your very own Pokémon: Pichu! This little
 let pokemonName;
 pokemonName = prompt("What will be Pichu's name?") || "Pichu";
 
-const re = /\D+$/i;
+const REGEX_POKEMON_NAME = /\D+$/i;
 
-while (!re.test(pokemonName)) {
+while (!REGEX_POKEMON_NAME.test(pokemonName)) {
   pokemonName = prompt("That's not a name! Choose another one");
 }
 
@@ -71,7 +78,7 @@ let food = getRandomInt();
 let rest = getRandomInt();
 let fun = getRandomInt();
 
-function mySubstraction(res, num) {
+function positiveNum (res, num) {
   let x = res - num;
   if (x < 0) {
     x = 0;
@@ -89,13 +96,6 @@ var events = ["Apparently today there's gonna be some crazy discounts at the Pok
 
 const days = [];
 
-class Day {
-  constructor(number, x) {
-    this.number = parseInt(number);
-    this.event = events[parseInt(x)];
-  }
-}
-
 days.push(new Day(1, 1));
 days.push(new Day(2, 2));
 days.push(new Day(3, 4));
@@ -106,13 +106,15 @@ days.push(new Day(7, 5));
 
 var dayEvents = days.map(days => "Day " + days.number + " - " + days.event)
 
+//Handles day to day events
 for (var index = 0; index <= 6; index++) {
   alert(dayEvents[index]);
+  // Handles daily hunger points
   if (food < 50) {
     const feedResponse = prompt(pokemonName + " seems to be hungry! Type FEED if you want to give him some treats!")?.toLowerCase();
     if (feedResponse === "feed") {
-      alert("He seems to love it!\n(+20 hunger points)");
       food = food + 40;
+      alert("He seems to love it!\n(+20 hunger points)");
       alert(food + " points of satiation!");
     }
     else if (feedResponse === "esc") {
@@ -120,21 +122,22 @@ for (var index = 0; index <= 6; index++) {
       break;
     }
     else {
+      food = positiveNum(food, 30);
       alert("Too bad! He seemed to be very hungry.");
-      food = mySubstraction(food, 30);
       alert(food + " points of satiation.");
     }
   }
   else {
-    alert("Pichu seems to be well fed lately. Good job!")
+    alert("Pichu seems to be well fed lately. Good job!");
   }
-  food = mySubstraction(food, 30);
+  food = positiveNum(food, 30);
 
+  // Handles daily rest points
   if (rest < 40) {
     const restResponse = prompt(pokemonName + " seems to be tired! Type REST if you want to cuddle him to sleep.")?.toLowerCase();
     if (restResponse === "rest") {
-      alert("He's fast asleep.\n(+20 rest points)");
       rest = rest + 40;
+      alert("He's fast asleep.\n(+20 rest points)");
       alert(rest + " points of rest!");
     }
     else if (restResponse === "esc") {
@@ -142,21 +145,22 @@ for (var index = 0; index <= 6; index++) {
       break;
     }
     else {
+      rest = positiveNum(rest, 20);
       alert("Poor thing, he seems to lack some sleep.");
-      rest = mySubstraction(rest, 20);
       alert(rest + " points of rest.");
     }
   }
   else {
-    alert("Pichu is energized as always. Look at him run!")
+    alert("Pichu is energized as always. Look at him run!");
   }
-  rest = mySubstraction(rest, 20);
+  rest = positiveNum(rest, 20);
 
+  // Handles daily fun points
   if (fun < 60) {
     const funResponse = prompt(pokemonName + " looks bored Type PLAY if you want to play with him!")?.toLowerCase();
     if (funResponse === "play") {
-      alert("He is having the time of his life!\n(+20 fun points)");
       fun = fun + 30;
+      alert("He is having the time of his life!\n(+20 fun points)");
       alert(fun + " points of fun!");
     }
     else if (funResponse === "esc") {
@@ -164,18 +168,18 @@ for (var index = 0; index <= 6; index++) {
       break;
     }
     else {
+      fun = positiveNum(fun, 20);
       alert("Maybe some other time...");
-      fun = mySubstraction(fun, 20);
       alert(fun + " points of fun.");
     }
   }
   else {
     alert("Pichu seems happy!")
   }
-  fun = mySubstraction(fun, 20);
+  fun = positiveNum(fun, 20);
   if (index == 7) {
     alert("You made it to the end of the week!");
   }
 }
 
-alert("Thank you for playing this short demo of the simulator!\nPress F5 to play again!")
+alert("Thank you for playing this short demo of the simulator!\nPress F5 to play again!");
