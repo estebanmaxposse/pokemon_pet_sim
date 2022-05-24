9fb1ee5a32add935fda25098c88e694c108822f9
estebanmaxposse <estebanposse98@gmail.com>
2022-05-24 19:53:28 -0300

    Added modals

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -110,30 +110,89 @@ class Day {
   }
 }
 
-alert("Pokémon Pet Simulator");
-alert("Hello there! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof!");
-alert("This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights. Myself… I study Pokémon as a profession.");
+var introModal = document.getElementById('intro-msg-1');
+
+$(window).on('load',function(){
+    $('#intro-msg-1').modal('show');
+});
+
+// alert("Pokémon Pet Simulator");
+// alert("Hello there! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof!");
+// alert("This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights. Myself… I study Pokémon as a profession.");
+
+var playerName = "Player";
 
 var pokemon1 = new Pokemon("Pichu", GENDER_MALE, "no_sprite_yet", 56, 1, 24, 56, 99);
 
-let playerName = prompt("So, what is your name?") || "Player";
-alert(`Right! So your name is ${playerName}!`);
+const introMessages = [
+  ["???", "Hello there! Welcome to the world of pokémon! My name is Oak! People call me the pokémon Prof!"],
+  ["Prof. Oak", "This world is inhabited by creatures called Pokémon! For some people, Pokémon are pets. Other use them for fights. Myself… I study Pokémon as a profession."],
+  ["Prof. Oak", "So, what is your name?"],
+  ["Prof. Oak", `Right! So your name is {playerName}!`],
+  ["Prof. Oak", `First, let me introduce you to your very own Pokémon: ${pokemon1.species}! This little boy is an electric type Pokémon. ${pokemon1.species}s are often social Pokémons known for their playful and mischievous demeanor.`],
+  ["Prof. Oak", `What will be ${pokemon1.species}'s name?`],
+  ["Prof. Oak", `So ${pokemon1.pokemonPronoun()} name is {pokemon1.customName}!`],
+  ["Prof. Oak", `{playerName}! {pokemon1.customName}! Your very own Pokémon legend is about to unfold! A world of dreams and adventures with Pokémon awaits! Let's go!`],
+]
 
-alert(`First, let me introduce you to your very own Pokémon: ${pokemon1.species}! This little boy is an electric type Pokémon. ${pokemon1.species}s are often social Pokémons known for their playful and mischievous demeanor.`);
+let msgIndex = 1;
+function continueIntro() {
+  if (msgIndex >= 8) {
+    $('#intro-msg-1').modal('hide');
+    return;
+  }
 
-pokemon1.customName = prompt(`What will be ${pokemon1.species}'s name?`) || pokemon1.species;
+  // Read user's input
+  if (msgIndex === 3 || msgIndex === 6) {
+    var nameInput = document.getElementById('player-name');
+    var name = nameInput.value;
+    if (msgIndex === 3) {
+      playerName = name || "Player";
+    }
+    else {
+      pokemon1.customName = name || "Pichu";
+    }
+    nameInput.value = "";
+  }
+
+  var introText = introMessages[msgIndex][1].replace("{playerName}", playerName)
+                                       .replace("{pokemon1.customName}", pokemon1.customName);
+
+  var newModalTitle = document.getElementById('intro-msg-1-toggle');
+  var modalText = document.getElementById('modal-text');
+
+  newModalTitle.innerText = introMessages[msgIndex][0];
+  modalText.innerText = introText;
+
+  // Allow user to write input
+  if (msgIndex === 2 || msgIndex === 5) {
+    document.getElementById('player-name-input').style.display = "block";
+  }
+  else {
+    document.getElementById('player-name-input').style.display = "none";
+  }
+
+  msgIndex++;
+}
+
+// let playerName = prompt("So, what is your name?") || "Player";
+// alert(`Right! So your name is ${playerName}!`);
+//
+// alert(`First, let me introduce you to your very own Pokémon: ${pokemon1.species}! This little boy is an electric type Pokémon. ${pokemon1.species}s are often social Pokémons known for their playful and mischievous demeanor.`);
+//
+// pokemon1.customName = prompt(`What will be ${pokemon1.species}'s name?`) || pokemon1.species;
 
 const REGEX_POKEMON_NAME = /\D+$/i;
 
-while (!REGEX_POKEMON_NAME.test(pokemon1.customName)) {
-  pokemon1.customName = prompt("That's not a name! Choose another one");
-}
+// while (!REGEX_POKEMON_NAME.test(pokemon1.customName)) {
+//   pokemon1.customName = prompt("That's not a name! Choose another one");
+// }
 
-alert(`So ${pokemon1.pokemonPronoun()} name is ${pokemon1.customName}!`);
-
-alert(`${playerName}! ${pokemon1.customName}! Your very own Pokémon legend is about to unfold! A world of dreams and adventures with Pokémon awaits! Let's go!`)
-
-alert("You can stop playing by typing ESC.")
+// alert(`So ${pokemon1.pokemonPronoun()} name is ${pokemon1.customName}!`);
+//
+// alert(`${playerName}! ${pokemon1.customName}! Your very own Pokémon legend is about to unfold! A world of dreams and adventures with Pokémon awaits! Let's go!`)
+//
+// alert("You can stop playing by typing ESC.")
 
 var events = [
   "Apparently today there's gonna be some crazy discounts at the PokeShop in town!",
@@ -158,86 +217,86 @@ days.push(new Day(7, 5));
 var dayEvents = days.map(days => "Day " + days.number + " - " + days.event)
 
 //Handles day to day events
-for (var index = 0; index < days.length; index++) {
-  let currentLevel = pokemon1.lv
-
-  alert(dayEvents[index]);
-  // Handles daily hunger points
-  if (pokemon1.hunger < 50) {
-    const feedResponse = prompt(`${pokemon1.customName} seems to be hungry! Type FEED if you want to give him some treats!`)?.toLowerCase();
-    if (feedResponse === "feed") {
-      pokemon1.addHunger(40);
-      alert("He seems to love it!\n(+20 hunger points)");
-      alert(pokemon1.hunger + " points of satiation!");
-    }
-    else if (feedResponse === "esc") {
-      alert("Thank you for playing!");
-      break;
-    }
-    else {
-      pokemon1.addHunger(-30);
-      alert("Too bad! He seemed to be very hungry.");
-      alert(pokemon1.hunger + " points of satiation.");
-    }
-  }
-  else {
-    alert(`${pokemon1.customName} seems to be well fed lately. Good job!`);
-  }
-  pokemon1.addHunger(-30);
-
-  // Handles daily rest points
-  if (pokemon1.rest < 40) {
-    const restResponse = prompt(`${pokemon1.customName} seems to be tired! Type REST if you want to cuddle him to sleep.`)?.toLowerCase();
-    if (restResponse === "rest") {
-      pokemon1.addRest(40);
-      alert("He's fast asleep.\n(+20 rest points)");
-      alert(pokemon1.rest + " points of rest!");
-    }
-    else if (restResponse === "esc") {
-      alert("Thank you for playing!");
-      break;
-    }
-    else {
-      pokemon1.addRest(-20);
-      alert(`Poor thing, ${pokemon1.pokemonPronoun()} seems to lack some sleep.`);
-      alert(pokemon1.rest + " points of rest.");
-    }
-  }
-  else {
-    alert(`${pokemon1.customName} is energized as always. Look at it run!`);
-  }
-  pokemon1.addRest(-20);
-
-  // Handles daily fun points
-  if (pokemon1.fun < 60) {
-    const funResponse = prompt(`${pokemon1.customName} looks bored Type PLAY if you want to play catch!`)?.toLowerCase();
-    if (funResponse === "play") {
-      pokemon1.addFun(30);
-      alert("You can feel the joy in the air!\n(+20 fun points)");
-      alert(pokemon1.fun + " points of fun!");
-    }
-    else if (funResponse === "esc") {
-      alert("Thank you for playing!");
-      break;
-    }
-    else {
-      pokemon1.addFun(-20);
-      alert("Maybe some other time...");
-      alert(pokemon1.fun + " points of fun.");
-    }
-  }
-  else {
-    alert(`${pokemon1.customName} seems happy!`)
-  }
-  pokemon1.addFun(-20);
-
-  if (currentLevel!=pokemon1.lv) {
-    alert(`${pokemon1.customName} leveled up! ${pokemon1.pokemonPronoun()} level is now ${pokemon1.lv}`);
-  }
-
-  if (index == days.length - 1) {
-    alert("You made it to the end of the week!");
-  }
-}
-
-alert("Thank you for playing this short demo of the simulator!\nPress F5 to play again!");
+// for (var index = 0; index < days.length; index++) {
+//   let currentLevel = pokemon1.lv
+//
+//   alert(dayEvents[index]);
+//   // Handles daily hunger points
+//   if (pokemon1.hunger < 50) {
+//     const feedResponse = prompt(`${pokemon1.customName} seems to be hungry! Type FEED if you want to give him some treats!`)?.toLowerCase();
+//     if (feedResponse === "feed") {
+//       pokemon1.addHunger(40);
+//       alert("He seems to love it!\n(+20 hunger points)");
+//       alert(pokemon1.hunger + " points of satiation!");
+//     }
+//     else if (feedResponse === "esc") {
+//       alert("Thank you for playing!");
+//       break;
+//     }
+//     else {
+//       pokemon1.addHunger(-30);
+//       alert("Too bad! He seemed to be very hungry.");
+//       alert(pokemon1.hunger + " points of satiation.");
+//     }
+//   }
+//   else {
+//     alert(`${pokemon1.customName} seems to be well fed lately. Good job!`);
+//   }
+//   pokemon1.addHunger(-30);
+//
+//   // Handles daily rest points
+//   if (pokemon1.rest < 40) {
+//     const restResponse = prompt(`${pokemon1.customName} seems to be tired! Type REST if you want to cuddle him to sleep.`)?.toLowerCase();
+//     if (restResponse === "rest") {
+//       pokemon1.addRest(40);
+//       alert("He's fast asleep.\n(+20 rest points)");
+//       alert(pokemon1.rest + " points of rest!");
+//     }
+//     else if (restResponse === "esc") {
+//       alert("Thank you for playing!");
+//       break;
+//     }
+//     else {
+//       pokemon1.addRest(-20);
+//       alert(`Poor thing, ${pokemon1.pokemonPronoun()} seems to lack some sleep.`);
+//       alert(pokemon1.rest + " points of rest.");
+//     }
+//   }
+//   else {
+//     alert(`${pokemon1.customName} is energized as always. Look at it run!`);
+//   }
+//   pokemon1.addRest(-20);
+//
+//   // Handles daily fun points
+//   if (pokemon1.fun < 60) {
+//     const funResponse = prompt(`${pokemon1.customName} looks bored Type PLAY if you want to play catch!`)?.toLowerCase();
+//     if (funResponse === "play") {
+//       pokemon1.addFun(30);
+//       alert("You can feel the joy in the air!\n(+20 fun points)");
+//       alert(pokemon1.fun + " points of fun!");
+//     }
+//     else if (funResponse === "esc") {
+//       alert("Thank you for playing!");
+//       break;
+//     }
+//     else {
+//       pokemon1.addFun(-20);
+//       alert("Maybe some other time...");
+//       alert(pokemon1.fun + " points of fun.");
+//     }
+//   }
+//   else {
+//     alert(`${pokemon1.customName} seems happy!`)
+//   }
+//   pokemon1.addFun(-20);
+//
+//   if (currentLevel!=pokemon1.lv) {
+//     alert(`${pokemon1.customName} leveled up! ${pokemon1.pokemonPronoun()} level is now ${pokemon1.lv}`);
+//   }
+//
+//   if (index == days.length - 1) {
+//     alert("You made it to the end of the week!");
+//   }
+// }
+//
+// alert("Thank you for playing this short demo of the simulator!\nPress F5 to play again!");
